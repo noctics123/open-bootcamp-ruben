d0f125996617e58d84f8e02b18673f79e4f83508
Ruben <59316090+noctics123@users.noreply.github.com>
2022-11-03 17:00:05 -0500

    Add files via upload

---
+++ Ejercicio-tema-3/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        funciones f = new funciones();
+       System.out.println("La suma es: "+f.suma(1,2,3));
+    }
+}
---
+++ Ejercicio-tema-3/src/funciones.java
@@ -0,0 +1,7 @@
+public class funciones {
+    public funciones() {
+    }
+     double suma(double a, double b, double c){
+        return a+b+c;
+    }
+}
