08b1cdd472c3abd241fb8ea3e7bd46c6002ff045
Ruben <59316090+noctics123@users.noreply.github.com>
2022-11-03 17:15:08 -0500

    Add files via upload

--- Ejercicio-tema-3/src/Main.java
+++ Ejercicio-tema-3/src/Main.java
@@ -1,7 +1,17 @@
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
+        //suma
         funciones f = new funciones();
        System.out.println("La suma es: "+f.suma(1,2,3));
+
+       //coche
+        coche c = new coche();
+        c.setNpuertas(Integer.parseInt(JOptionPane.showInputDialog("Ingrese puertas del coche")));
+        c.addpuertas();
+        System.out.println("El coche ahora tiene "+c.getNpuertas()+" puertas");
+
     }
 }
---
+++ Ejercicio-tema-3/src/coche.java
@@ -0,0 +1,18 @@
+public class coche {
+    int npuertas;
+
+    public coche() {
+    }
+
+    public int getNpuertas() {
+        return npuertas;
+    }
+
+    public void setNpuertas(int npuertas) {
+        this.npuertas = npuertas;
+    }
+
+    void addpuertas(){
+        npuertas++;
+    }
+}
